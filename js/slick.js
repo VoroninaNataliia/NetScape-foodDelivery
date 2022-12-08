804ef297327b2f5c18fa59c3c7a1c09e918262ee
VoroninaNataliia <voronina.nataliya0702@gmail.com>
2022-12-08 14:14:40 +0100

    js

--- js/slick.js
+++
@@ -1,9 +0,0 @@
-$('.one-time').slick({
-  // dots: true,
-  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
-  infinite: true,
-  speed: 400,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-});
